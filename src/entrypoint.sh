057b4d160421774579eee6de94a2f12e4c6d75f4
Guan <guanlinc@stud.ntnu.no>
2020-04-11 03:45:16 +0200

    Enable SSL connections

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -29,20 +29,24 @@ content_server=$content_server'}\n'
 
 # Configuration skeleton for using SSL
 # https://nginx.org/en/docs/http/configuring_https_servers.html
-#content_server=$content_server'server {\n'
-#content_server=$content_server"    listen 443 ssl http2;\n"
+content_server=$content_server'server {\n'
+content_server=$content_server"    listen 443 ssl http2;\n"
 #content_server=$content_server'    ssl_certificate      <certificate .crt file>;\n'
+content_server=$content_server'    ssl_certificate          /etc/ssl/certs/nginx-selfsigned.crt;\n'
 #content_server=$content_server'    ssl_certificate_key     <certificate .key file>;\n'
-#content_server=$content_server'    ssl_protocols       <optional protocols>;\n'
-#content_server=$content_server'    ssl_ciphers         <optional ciphers>;\n'
-#content_server=$content_server'    location / {\n'
-#content_server=$content_server'        include uwsgi_params;\n'
-#content_server=$content_server'        uwsgi_pass unix:///tmp/uwsgi.sock;\n'
-#content_server=$content_server'    }\n'
-#content_server=$content_server"    location $USE_STATIC_URL {\n"
-#content_server=$content_server"        alias $USE_STATIC_PATH;\n"
-#content_server=$content_server'    }\n'
-#content_server=$content_server'}\n'
+content_server=$content_server'    ssl_certificate_key     /etc/ssl/private/nginx-selfsigned.key;\n'
+content_server=$content_server'    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;\n'
+content_server=$content_server'    ssl_ciphers         HIGH:!aNULL:!MD5;\n'
+content_server=$content_server'    location / {\n'
+content_server=$content_server'        include uwsgi_params;\n'
+content_server=$content_server'        uwsgi_pass unix:///tmp/uwsgi.sock;\n'
+content_server=$content_server'        add_header X-Frame-Options \"sameorigin\" always;\n'
+content_server=$content_server'        add_header X-Content-Type-Options \"nosniff\";\n'
+content_server=$content_server'    }\n'
+content_server=$content_server"    location $USE_STATIC_URL {\n"
+content_server=$content_server"        alias $USE_STATIC_PATH;\n"
+content_server=$content_server'    }\n'
+content_server=$content_server'}\n'
 
 
 # Save generated server /etc/nginx/conf.d/nginx.conf
